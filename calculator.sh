56889604d0296eae1479982d1eec255fa90b7958
root <root@ip-172-31-90-35.ec2.internal>
2024-07-20 14:17:41 +0000

    c3

--- calculator.sh
+++ calculator.sh
@@ -1,12 +1,16 @@
-a=10
+aaa=10
 b=20
 c=$((a+b))
 d=$((a-b))
 e=$((a*b))
 f=$((a/b))
 echo "addition is $c"
-echo "subraction is $d"
+echobraction is $d"
 echo "multiply is $e"
 echo "divide is $f"
 hello azure
+<<<<<<< Updated upstream
 gcp
+=======
+aws
+>>>>>>> Stashed changes
